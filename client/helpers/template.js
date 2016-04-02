194ffb49b67087d6bcc3a0b9ec934e51556154fc
jordanow <shailendrapal100@gmail.com>
2016-04-02 19:27:58 +1100

    Fix urls and alerts

---
+++ client/helpers/template.js
@@ -0,0 +1,9 @@
+Bert.defaults = {
+  hideDelay: 3500,
+  // Accepts: a number in milliseconds.
+  style: 'growl-bottom-right',
+  // Accepts: fixed-top, fixed-bottom, growl-top-left,   growl-top-right,
+  // growl-bottom-left, growl-bottom-right.
+  type: 'default'
+    // Accepts: default, success, info, warning, danger.
+};
